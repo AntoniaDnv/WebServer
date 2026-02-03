474ff16177815c0f39acc71932edbb5b7219ead5
AntoniaDnv <antonia.d.neva@gmail.com>
2026-02-03 21:03:08 +0200

    web server started

---
+++ WebServer-First/WebServer-First.Demo/Program.cs
@@ -0,0 +1,13 @@
+﻿using WebServer_First.Server;
+
+namespace WebServer_First.Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new HttpServer("127.0.0.1", 8080);
+            server.Start();
+        }
+    }
+}
---
+++ WebServer-First/WebServer-First.Server/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace WebServer_First.Server
+{
+    public class HttpServer
+    {
+        private readonly IPAddress iPAddress;
+        private readonly int port;
+        private readonly TcpListener serverListener;
+
+        public HttpServer(string ipAddress, int port)
+        {
+            this.iPAddress = IPAddress.Parse(ipAddress);
+            this.port = port;
+            this.serverListener = new TcpListener(this.iPAddress, this.port);    
+        }
+
+        public void Start()
+        {
+            serverListener.Start();
+            Console.WriteLine("Srever started");
+            Console.WriteLine($"Listening on port: {port}");
+            while (true)
+            {
+                var connection = serverListener.AcceptTcpClient();
+                var networkStream = connection.GetStream();
+
+                string content = "Hello from the server!";
+                WriteResponse(networkStream, content);
+                
+                connection.Close();
+            }
+        }
+
+        private void WriteResponse(NetworkStream networkStream, string message)
+        {
+            int contentLength = Encoding.UTF8.GetByteCount(message);
+            var response = $@"HTTP/1.1
+Content-Type:text/plain; charset=UTF-8
+Content-Length: {contentLength}
+
+{message}";
+            var responseBytes = Encoding.UTF8.GetBytes(response);
+            networkStream.Write(responseBytes);
+        }
+    }
+
+}
+
+  
+

8d85eaecc3dabfd4c4cf8ef06d03e2d010455746
AntoniaDnv <antonia.d.neva@gmail.com>
2026-02-03 10:36:22 +0200

    my first web server init commit

---
+++ WebServer-First/WebServer-First/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WebServer_First
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
